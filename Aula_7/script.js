79b6a2a58b2821a80d8d25a7f9ad0a4ea57a93d3
Fagner Pimentel <fagnerpimentel@gmail.com>
2025-04-08 14:00:33 -0300

    Exemplos de canvas

---
+++ Aula_7/script.js
@@ -0,0 +1,53 @@
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+
+// retângulos
+ctx.beginPath();
+ctx.lineWidth = 5;
+ctx.fillStyle = 'blue';
+ctx.strokeStyle = 'red';
+ctx.fillRect(0,0,70,70);
+ctx.strokeRect(70,70,50,50);
+ctx.closePath();
+
+// linhas
+ctx.beginPath();
+ctx.lineWidth = 5;
+ctx.fillStyle = 'blue';
+ctx.strokeStyle = 'red';
+ctx.moveTo(200,150);
+ctx.lineTo(120,120);
+ctx.lineTo(60,250);
+ctx.lineTo(200,250);
+ctx.lineTo(200,150);
+ctx.fill();
+ctx.stroke();
+ctx.closePath();
+
+	// arcos
+ctx.beginPath();
+ctx.lineWidth = 5;
+ctx.fillStyle = 'blue';
+ctx.strokeStyle = 'red';
+ctx.arc(200,100,50,0*Math.PI,2*Math.PI);
+ctx.fill();
+ctx.stroke();
+ctx.closePath();
+
+
+// texto
+ctx.beginPath();
+ctx.lineWidth = 5;
+ctx.fillStyle = 'blue';
+ctx.strokeStyle = 'red';
+ctx.font = "90px Arial"
+ctx.textAlign = "center";
+ctx.strokeText("Olá",200,350)
+ctx.fillText("Olá",195,350);
+ctx.closePath();
+
+
+
+let canvas2 = document.getElementById('canvas2');
+let ctx2 = canvas2.getContext('2d');
+
