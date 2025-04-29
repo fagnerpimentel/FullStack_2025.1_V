e728a47d394ed3c8d36e4cfc920d2b5cecd1c025
Fagner Pimentel <fagnerpimentel@gmail.com>
2025-04-29 16:07:52 -0300

    Exemplos de Banco de dados: Create e Read

--- Servidor/servidor.js
+++ Servidor/servidor.js
@@ -1,7 +1,15 @@
 require("colors");
 var http = require("http");
 var express = require("express");
-let bodyParser = require("body-parser")
+var bodyParser = require("body-parser")
+var mongodb = require("mongodb");
+
+const MongoClient = mongodb.MongoClient;
+const uri = `mongodb+srv://fagner:KhMX80NtBAtJJKCD@aulabd.cjihvk4.mongodb.net/?retryWrites=true&w=majority&appName=AulaBD`;
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+var dbo = client.db("exemplo_bd");
+var usuarios = dbo.collection("usuarios");
 
 var app = express();
 app.use(express.static('./public'))
@@ -41,5 +49,39 @@ app.post("/cadastrar", function(requisicao, resposta){
     let senha = requisicao.body.senha;
     let nasc = requisicao.body.nascimento;
     console.log(nome, login, senha, nasc);
-    resposta.render("resposta",{nome, login, senha, nasc});
+
+    var data = { db_nome: nome, db_login: login, db_senha: senha, db_nasc: nasc };
+
+    usuarios.insertOne(data, function(err){
+        if(err){
+            resposta.render("resposta",{status: "Erro" ,nome, login, senha, nasc});
+        }else{
+            resposta.render("resposta",{status: "Sucesso", nome, login, senha, nasc});
+        }
+    })
+})
+
+app.get("/for_ejs",function(requisicao, resposta){
+    let valor = requisicao.query.valor;
+    resposta.render("exemplo_for",{valor});
+})
+
+app.post('/logar', function(requisicao, resposta){
+    let login = requisicao.body.login;
+    let senha = requisicao.body.senha;
+    console.log(login, senha);
+
+    var data = {db_login: login, db_senha: senha}
+
+    usuarios.find(data).toArray(function(err, items){
+        console.log(items)
+        if(items.length == 0){
+            resposta.render("resposta_login",{status: "usuario/senha não encontrado"});
+        }else if(err){
+            resposta.render("resposta_login",{status: "erro ao logar"});
+        }else{
+            resposta.render("resposta_login",{status: "usuario "+login+" logado"});
+        }
+    })
+
 })
